707a3d2ee3b900daf626fe913dcde76ab83b08df
Fernando <fernandoz@outlook.com>
2018-06-06 13:43:37 -0700

    Remove favicon

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-      favicon: "./public/favicon.ico"
+      
     })
   ]
 };
